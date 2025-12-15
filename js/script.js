99651d92343dc479f1edd428cffb416f1a1a816e
banmala <banmalasunil@gmail.com>
2025-12-15 23:37:27 +0545

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.onload=()=>{
   toggleNavbar();
   toggleMobileNav();
+  responsingBgWithMouseMovement();
 }
 
 // Setup Navbar Scroll Effect
@@ -44,4 +45,46 @@ const toggleMobileNav = () => {
 
     navToggler.addEventListener('click', togglerClick);
     navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
+
+const responsingBgWithMouseMovement = () => {
+  var xpFirst = 0, mouseX = 0;
+  var ypFirst = 0, mouseY = 0;
+  var xpSecond = 0, mouseX = 0;
+  var ypSecond = 0, mouseY = 0;
+  var xpThird = 0, mouseX = 0;
+  var ypThird = 0, mouseY = 0;
+
+  const cursorFollowerFirst = document.querySelector(".cursorFollowerFirst");
+  const cursorFollowerSecond= document.querySelector(".cursorFollowerSecond");
+  const cursorFollowerThird = document.querySelector(".cursorFollowerThird");
+
+  document.addEventListener("mousemove", (e) => {
+    mouseX = e.screenX;
+    mouseY = e.screenY;
+  });
+
+  setInterval(function () {
+    xpFirst += (mouseX - xpFirst) / 60;
+    ypFirst += (mouseY - ypFirst) / 60;
+
+    cursorFollowerFirst.style.left = xpFirst + "px";
+    cursorFollowerFirst.style.top = ypFirst + "px";
+  }, 20);
+
+  setInterval(function () {
+    xpSecond += (mouseX - xpSecond) / 80;
+    ypSecond += (mouseY - ypSecond) / 80;
+
+    cursorFollowerSecond.style.left = xpSecond + "px";
+    cursorFollowerSecond.style.top = ypSecond + "px";
+  }, 20);
+
+  setInterval(function () {
+    xpThird += (mouseX - xpThird) / 85;
+    ypThird += (mouseY - ypThird) / 85;
+
+    cursorFollowerThird.style.left = xpThird + "px";
+    cursorFollowerThird.style.top = ypThird + "px";
+  }, 20);
 }
