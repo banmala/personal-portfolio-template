ce702f7914a88c0dd57d465ec541a588f6eceda0
banmala <banmalasunil@gmail.com>
2025-12-19 21:45:48 +0545

    Fixed mobile menu itsma as per navmenu items

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ window.onload=()=>{
   toggleNavbar();
   toggleMobileNav();
   responsingBgWithMouseMovement();
+  navMobile();
 }
 
 // Setup Navbar Scroll Effect
@@ -16,18 +17,36 @@ const toggleNavbar = () => {
     });
 };
 
+// Setup Mobile Navigation Menu Items
+const navMobile = () =>{
+  const navMenuItems = document.getElementById('nav-menu-items');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const children = navMenuItems.children;
+  for(let i=0; i<children.length; i++){
+    const menuItem = document.createElement('a');
+    menuItem.href = children[i].href;
+    menuItem.className = 'block px-4 py-2 text-5xl';
+    menuItem.textContent = children[i].textContent;
+    mobileMenu.appendChild(menuItem);
+  }
+}
+
+// Setup Mobile Navigation Toggle
 const toggleMobileNav = () => {
     const navToggler = document.querySelector('#nav-toggler');
     const navMenu = document.querySelector('#mobile-menu');
     const navLinks = document.querySelectorAll('#mobile-menu a');
+    navToggler.innerHTML = '<i class="fas fa-bars text-2xl"></i>';
     // togglerClick function
     function togglerClick() {
         if(navMenu.classList.contains('opacity-0')) {
-        navMenu.classList.remove('opacity-0');
-        navMenu.classList.add('opacity-100');
+          navMenu.classList.remove('opacity-0');
+          navMenu.classList.add('opacity-100');
+          navToggler.innerHTML = '<i class="fas fa-x text-2xl"></i>';
         }else{
-        navMenu.classList.remove('opacity-100');
-        navMenu.classList.add('opacity-0');
+          navMenu.classList.remove('opacity-100');
+          navMenu.classList.add('opacity-0');
+          navToggler.innerHTML = '<i class="fas fa-bars text-2xl"></i>';
         }
     }
     // navLinkClick function
@@ -36,10 +55,12 @@ const toggleMobileNav = () => {
             navToggler.click();
         }
     }
+    // Hide mobile menu on scroll
     window.addEventListener('scroll', (e) => {
         if (window.scrollY > 50 && navMenu.classList.contains('opacity-100')) {
             navMenu.classList.remove('opacity-100');
             navMenu.classList.add('opacity-0');
+            navToggler.innerHTML = '<i class="fas fa-bars text-2xl"></i>';
         }
     });
 
@@ -47,6 +68,7 @@ const toggleMobileNav = () => {
     navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
 }
 
+// Setup Background Animation with Mouse Movement
 const responsingBgWithMouseMovement = () => {
   var xpFirst = 0, mouseX = 0;
   var ypFirst = 0, mouseY = 0;
@@ -54,11 +76,9 @@ const responsingBgWithMouseMovement = () => {
   var ypSecond = 0, mouseY = 0;
   var xpThird = 0, mouseX = 0;
   var ypThird = 0, mouseY = 0;
-
   const cursorFollowerFirst = document.querySelector(".cursorFollowerFirst");
   const cursorFollowerSecond= document.querySelector(".cursorFollowerSecond");
   const cursorFollowerThird = document.querySelector(".cursorFollowerThird");
-
   document.addEventListener("mousemove", (e) => {
     mouseX = e.screenX;
     mouseY = e.screenY;
@@ -67,7 +87,6 @@ const responsingBgWithMouseMovement = () => {
   setInterval(function () {
     xpFirst += (mouseX - xpFirst) / 60;
     ypFirst += (mouseY - ypFirst) / 60;
-
     cursorFollowerFirst.style.left = xpFirst + "px";
     cursorFollowerFirst.style.top = ypFirst + "px";
   }, 20);
@@ -75,7 +94,6 @@ const responsingBgWithMouseMovement = () => {
   setInterval(function () {
     xpSecond += (mouseX - xpSecond) / 80;
     ypSecond += (mouseY - ypSecond) / 80;
-
     cursorFollowerSecond.style.left = xpSecond + "px";
     cursorFollowerSecond.style.top = ypSecond + "px";
   }, 20);
@@ -83,7 +101,6 @@ const responsingBgWithMouseMovement = () => {
   setInterval(function () {
     xpThird += (mouseX - xpThird) / 85;
     ypThird += (mouseY - ypThird) / 85;
-
     cursorFollowerThird.style.left = xpThird + "px";
     cursorFollowerThird.style.top = ypThird + "px";
   }, 20);
